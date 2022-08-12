c11b0a1d579db947aa277ab77b9a0efd90e83bd2
Marton Cserei <marton.cserei@nanolabs.ro>
2022-08-12 19:02:04 +0300

    test1

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ app.get("/", (req, res) => {
   res.send("Server is up!");
 });
 
-app.post("/", handlePush);
+app.post("/", (req, res) => {
+  const push = req.body;
+  handlePush(push);
+  res.send();
+});
 
 function startApp() {
   setupGit();
@@ -19,24 +23,23 @@ function startApp() {
   });
 }
 
-function handlePush(req, res) {
-  const push = req.body;
-  const pusher = push.pusher.name;
-  const branch = push.ref.replace("refs/heads/", "");
+function handlePush(push) {
+  setTimeout(() => {
+    const pusher = push.pusher.name;
+    const branch = push.ref.replace("refs/heads/", "");
 
-  const testResults = runTests();
-  const failCount = testResults.total - testResults.passed;
+    const testResults = runTests();
+    const failCount = testResults.total - testResults.passed;
 
-  if (failCount) {
-    const fails = testResults.failedTests.join();
-    console.log(
-      `The last push on ${branch} made by ${pusher} has ${failCount} tests failing. Check out them: \`${fails}\``
-    );
-  } else {
-    console.log(`Push on ${branch} by ${pusher}. All good.`);
-  }
-
-  res.send();
+    if (failCount) {
+      const fails = testResults.failedTests.join();
+      console.log(
+        `The last push on ${branch} made by ${pusher} has ${failCount} tests failing. Check out them: \`${fails}\``
+      );
+    } else {
+      console.log(`Push on ${branch} by ${pusher}. All good.`);
+    }
+  });
 }
 
 startApp();
