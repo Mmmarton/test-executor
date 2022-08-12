1b7635a242ca641702f0cdd6ef52d45f1baa3ba0
Marton Cserei <marton.cserei@nanolabs.ro>
2022-08-12 18:56:52 +0300

    add test runner

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { setupGit, runTests } = require("./test-runner");
 const app = express();
 const port = 3000;
 
@@ -8,13 +9,34 @@ app.get("/", (req, res) => {
   res.send("Server is up!");
 });
 
-app.post("/", (req, res) => {
-  const push = req.body;
-  const branch = push.ref.replace("refs/heads/", "");
-  console.log({ pusher: push.pusher.name, branch });
-  res.send();
-});
+app.post("/", handlePush);
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+function startApp() {
+  setupGit();
+
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
+}
+
+function handlePush(req, res) {
+  const push = req.body;
+  const pusher = push.pusher.name;
+  const branch = push.ref.replace("refs/heads/", "");
+
+  const testResults = runTests();
+  const failCount = testResults.total - testResults.passed;
+
+  if (failCount) {
+    const fails = testResults.failedTests.join();
+    console.log(
+      `The last push on ${branch} made by ${pusher} has ${failCount} tests failing. Check out them: \`${fails}\``
+    );
+  } else {
+    console.log(`Push on ${branch} by ${pusher}. All good.`);
+  }
+
+  res.send();
+}
+
+startApp();
---
+++ test-runner.js
@@ -0,0 +1,25 @@
+const shell = require("shelljs");
+
+function setupGit() {
+  if (!shell.which("git")) {
+    shell.echo("This script requires git");
+    shell.exit(1);
+  }
+}
+
+function runTests() {
+  shell.exec("npm run test");
+
+  const results = require("./test-results.json");
+  const failedTests = results.testResults[0].assertionResults
+    .filter(({ status }) => status === "failed")
+    .map((test) => test.fullName);
+
+  return {
+    total: results.numTotalTests,
+    passed: results.numPassedTests,
+    failedTests,
+  };
+}
+
+module.exports = { setupGit, runTests };
